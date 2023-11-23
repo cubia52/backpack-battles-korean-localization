8efddccabcaadea9605ee26b6342088364c041db
cubia52 <151850935+cubia52@users.noreply.github.com>
2023-11-23 23:54:07 +0900

    Update patch.bat

--- patch.bat
+++ patch.bat
@@ -1,6 +1,6 @@
 @echo OFF
 
-SET "GAME_HOME=%ProgramFiles(x86)%\Steam\steamapps\common\Backpack Battles Demo" || goto :error
+SET "E:\Steam\steamapps\common\Backpack Battles Demo" || goto :error
 godotpcktool\godotpcktool.exe --pack "%GAME_HOME%\BackpackBattles.pck" --action add --remove-prefix overrides --file overrides || goto :error
 echo Patch done!
 pause
